92093ae3b235db613c41966724cf6a35060e8e68
rezky <88895489+phantom43@users.noreply.github.com>
2022-02-24 18:18:18 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,45 @@
+import os
+
+def talk():
+	os.system("clear")
+	print("check your name and password")
+	os.system("sleep 2")
+	os.system("clear")
+	print("correct :) redirecting to admin system[      ]")
+	os.system("sleep 2")
+	os.system("clear")
+	print("correct :) redirecting to admin system[=     ]")
+	os.system("sleep 1")
+	os.system("clear")
+	print("correct :) redirecting to admin system[==    ]")
+	os.system("sleep 1")
+	os.system("clear")
+	print("correct :) redirecting to admin system[===   ]")
+	os.system("sleep 1")
+	os.system("clear")
+	print("correct :) redirecting to admin system[====  ]")
+	os.system("sleep 1")
+	os.system("clear")
+	print("correct :) redirecting to admin system[===== ]")
+	os.system("sleep 1")
+	os.system("clear")
+	print("correct :) redirecting to admin system[======]")
+	os.system("sleep 1")
+	os.system("clear")
+	print("correct :) redirecting to admin system[ DONE ]")
+	os.system("sleep 1")
+	os.system("clear")
+def options():
+	print("""
++-----------------------------+
+|           OPTIONS           |
++-----------------------------+
+| [1] CHECK ALL DATA          |
+| [2] CHECK NAME              | 
+| [3] CHECK DATES             |
+| [4] CHECK EMAIL             |
+|                             |
++-----------------------------+
+|       GIT : phantom43       | v.1.0.0
++-----------------------------+
+			""")
